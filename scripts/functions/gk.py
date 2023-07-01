b82c33e78e5bebe5e314588ca271c2bf71fe2dcd
Emi Cervantes <91242164+emicervantes@users.noreply.github.com>
2023-06-30 21:45:42 -0700

    updated function

--- scripts/functions/gk.py
+++ scripts/functions/gk.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def gk(A, x, y, TOL):
   k=1
@@ -5,7 +6,7 @@ def gk(A, x, y, TOL):
   x_old = np.zeros(n)
   x_lst = [x_old]
   ap_error = []
-  ar = approximation_error(x_old, x)
+  ar =  (np.linalg.norm(x_old-x))**2
   ap_error.append(ar)
   while True:
     r = []
@@ -17,7 +18,7 @@ def gk(A, x, y, TOL):
     x_new = x_old - ((np.transpose(ai)@x_old - y[i]) * ai)
     x_lst.append(x_new)
     x_old = x_new
-    ar = approximation_error(x_old, x)
+    ar =  (np.linalg.norm(x_old-x))**2
     ap_error.append(ar)
     k+=1
     if ar < TOL:
